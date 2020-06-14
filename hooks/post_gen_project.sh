e35a63b3de1d7567a221a671ef0b4815e3c69684
Brian Pugh <bnp117@gmail.com>
2020-06-14 16:01:14 -0700

    Bump version: 0.3.1 → 0.3.2

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,6 +6,6 @@
 git init
 git add .
 ( rmdir components/esp_littlefs && git submodule add https://github.com/joltwallet/esp_littlefs.git components/esp_littlefs && git submodule update --init --recursive ) || true
-git commit -a -m "Initial Cookiecutter Commit from github.com:BrianPugh/cookiecutter-esp32-webserver.git ( 0.3.1 )."
+git commit -a -m "Initial Cookiecutter Commit from github.com:BrianPugh/cookiecutter-esp32-webserver.git ( 0.3.2 )."
 
 git remote add origin {{cookiecutter.git_repo}}
